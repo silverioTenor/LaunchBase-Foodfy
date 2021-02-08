a79a4c313a503a5c1e31a31fe43dd148a6e22846
silverioTenor <silverio_tenor@outook.com>
2021-02-08 20:38:05 -0300

    Implementação da página de visualização da receita. Estrutura + estilização.

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -5,6 +5,9 @@ const HomeController = {
   recipes(request, response) {
     return response.render('public/recipes');
   },
+  show(request, response) {
+    return response.render('public/show');
+  },
   about(request, response) {
     return response.render('public/about');
   },
--- src/routes/public/index.js
+++ src/routes/public/index.js
@@ -5,6 +5,7 @@ const HomeController = require('../../app/controllers/HomeController');
 
 publicRoutes.get('/', HomeController.home);
 publicRoutes.get('/recipes', HomeController.recipes);
+publicRoutes.get('/recipes/show', HomeController.show);
 publicRoutes.get('/about', HomeController.about);
 publicRoutes.get('/chefs', HomeController.chefs);
 
