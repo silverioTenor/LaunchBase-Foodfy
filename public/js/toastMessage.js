3d52d9ac4197e77441086f6567a303f6a5124624
silverioTenor <silverio_tenor@outook.com>
2021-02-10 19:27:38 -0300

    Implementação das mensagens de toast na aplicação.

---
+++ public/js/toastMessage.js
@@ -0,0 +1,16 @@
+const ToastMessage = {
+  messageContainer: document.querySelector('.message-container'),
+  dismiss() {
+    const message = ToastMessage.messageContainer.classList;
+    if (message.contains('dismiss')) {
+      message.remove('dismiss');
+    }
+    setTimeout(() => {
+      message.add('dismiss');
+    }, 3000);
+  }
+}
+
+if (document.querySelector('.message-container')) {
+  ToastMessage.dismiss();
+}
